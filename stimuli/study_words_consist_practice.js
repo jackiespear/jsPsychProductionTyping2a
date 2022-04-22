4e3e04b352d368a76a00cef1f03be25024b7be79
Jackie Spear <jackiespear44@gmail.com>
2022-04-22 10:51:01 -0500

    Adding exp files
    high/low freq words, firebase updated

---
+++ stimuli/study_words_consist_practice.js
@@ -0,0 +1,4 @@
+studywords_consist_practice = [
+{"stimulus":"<p style='font-size: 48px;'>EACH</p>","data":{"type":"practice","category":"consistent","word":"EACH"}},
+{"stimulus":"<p style='font-size: 48px;'>SAID</p>","data":{"type":"practice","category":"consistent","word":"SAID"}},
+]
---
+++ stimuli/study_words_inconsist_practice.js
@@ -0,0 +1,4 @@
+studywords_inconsist_practice = [
+{"stimulus":"<p style='font-size: 48px;'>YACHT</p>","data":{"type":"practice","category":"inconsistent","word":"YACHT"}},
+{"stimulus":"<p style='font-size: 48px;'>PLAID</p>","data":{"type":"practice","category":"inconsistent","word":"PLAID"}},
+]
